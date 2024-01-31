8a87a06147ca46e7088a9594a9bca2bef15d9452
YevShch <yevheniiashcherbakova82@gmail.com>
2024-01-31 14:25:12 +0100

    updated ticked-price.feature
    fixed the calculation of the total price

--- specs/step_definitions/ticket-price.js
+++ specs/step_definitions/ticket-price.js
@@ -3,10 +3,9 @@ import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 When( 'I choose the screening from the list with date {string}', ( date ) => {
   // TODO: implement step
   cy.get( 'div.accordion-item' )
-    .should( 'be.visible' )
     .find( '.screening-list' )
-    .contains(date)
-    .click({force:true})
+    .contains( date )
+    .click()
 } );
 
 
@@ -15,9 +14,7 @@ Then('I should see the price of {string} for a seat for {string}', (price, categ
   cy.get( 'div.ticket-category' )
     .contains( categoryName )
     .parent()
-    .should( 'be.visible' )
     .find( 'label:last' )
-    .should( 'be.visible' )
     .invoke( 'text' ).then( ( text ) => {
     const expectedPrice = text.trim();
       cy.log( expectedPrice )
@@ -25,45 +22,31 @@ Then('I should see the price of {string} for a seat for {string}', (price, categ
   })
 });
 
-Then('I should see the total price of {string}', (totalPrice) => {
+Then( 'I should see the total price of {string}', ( totalPrice ) => {
   // TODO: implement step
-  cy.get( 'p.total-price' )
-    .should( 'be.visible' )
-    .find( '.total-value' )
-    .invoke( 'text' ).then( ( text ) => {
-    const expectedTotalPrice = text.trim();
-  cy.log( expectedTotalPrice )
-  expect( totalPrice ).to.equal( expectedTotalPrice )
- } )
-});
+  cy.get( 'p.total-price .total-value' ).invoke( 'text' ).then( ( text ) => {
+      const expectedTotalPrice = text.trim();
+      cy.log( expectedTotalPrice )
+      expect( totalPrice ).to.equal( expectedTotalPrice )
+    } )
+} );
 
-Then('I should get the booking confirmation message contains the ticket price {string}', (a) => {
+Then( 'the total price should be calculated correctly and equal to the sum of the ticket prices', () => {
   // TODO: implement step
+  let sumRowPrice = 0;
+  cy.get( '.ticket-category' ).each( ( $el ) => {
+    const $units = $el.find( 'span' ).text().replace( /\D/g, '' );
+    const $categoryPrice = $el.find( 'label:last' ).text().replace( /\D/g, '' );
 
-});
-
-Then('the total price should be calculated correctly and equal to the sum of the ticket prices', () => {
-  // TODO: implement step
-  cy.get( 'div.ticket-category' )
-    .should( 'be.visible' )
-    .find( 'label:last' )
-    .should( 'be.visible' )
-    .then( ( $rowPrice ) => {
-
- let sumRowPrice = 0
-   for ( let i = 0; i < $rowPrice.length; i++ ) {
-   sumRowPrice += parseFloat( $rowPrice[ i ].innerText )
- cy.log( sumRowPrice )
-      }
-  cy.get( 'p.total-price' )
-   .should( 'be.visible' )
-   .find( '.total-value' )
-   .invoke( 'text' ).then( ( text ) => {
-
-     const expectedTotalPrice = text.trim()
-     cy.log( expectedTotalPrice )
+    let rowPrice = $categoryPrice * $units;
+    cy.log( rowPrice );
+    sumRowPrice += rowPrice;
+    cy.log( sumRowPrice );
+  } );
+  cy.get( 'p.total-price .total-value' ).invoke( 'text' ).then( ( text ) => {
      
-  expect( expectedTotalPrice ).to.equal( sumRowPrice )
-   } )
- })       
-});
+    const expectedTotalPrice = text.replace( /\D/g, '' );
+      cy.log( expectedTotalPrice )
+      assert.equal( expectedTotalPrice, sumRowPrice )
+    } )
+} );
