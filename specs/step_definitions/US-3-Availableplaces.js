d04a664a3838c08899d64e6ecc2dee2b01c0018f
Esra Unal <118307139+esraunal07@users.noreply.github.com>
2024-02-03 00:04:54 +0100

    v3

---
+++ specs/step_definitions/US-3-Availableplaces.js
@@ -0,0 +1,56 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Given('the user is on the websites home page', () => {
+  // TODO: implement step
+  cy.visit('https://filmvisarna-team5.nodehill.se/');
+});
+
+Given('user selects {string} and {string} and then clicks on the button {string}', (a, b, c) => {
+  // TODO: implement step
+  cy.wait(4000);
+  cy.get(':nth-child(1) > .movieImages').first().click({force: true});
+  //cy.get(':nth-child(1) > .screeningContainer > .screeningsBtn');
+  cy.get(':nth-child(2) > .custom-radio-button').click({force: true} );
+  cy.get('.screening-list > .confirm-button').click({force: true} );
+});
+
+Then('the user selects the {string} and {string} and clicks on the button {string}', (a, b, c) => {
+  // TODO: implement step
+  cy.get(':nth-child(1) > .counter-container > :nth-child(3)').click({force: true});
+  //cy.get('.ticket-list-item .button-container').last().find('button').last().click()
+  cy.get('.price-component > .confirm-button').click({force: true});
+});
+
+Then('the system displays the available {string} for the selected showtime.', (a) => {
+  // TODO: implement step
+  cy.get('.theater-container');
+  cy.screenshot('.theater-container');
+  cy.get('.theater-container').should('exist');
+});
+
+Then('the user clicks on one of the {string}.', (a) => {
+  // TODO: implement step
+  cy.get(':nth-child(6) > :nth-child(3)').click({force: true}); 
+});
+
+Then('the user verify that {string} are visible.', (a) => {
+  // TODO: implement step
+  cy.get('.theater-container > :nth-child(2) > :nth-child(3)');
+  cy.screenshot('.theater-container > :nth-child(2) > :nth-child(3)');
+});
+
+Then('the user selects the {string} and {string} and clicks on the button {string}.', (a, b, c) => {
+  // TODO: implement step
+  cy.get(':nth-child(1) > .counter-container > :nth-child(3)').click({force: true});
+  cy.get('.price-component > .confirm-button').click({force: true});
+});
+
+Then('the system notifies that all seats are {string}.', (a) => {
+  // TODO: implement step
+  cy.get('.seats-explained > :nth-child(3)').contains('Upptagen plats'); 
+});
+
+Then('the user verify that no seats are visible.', () => {
+  // TODO: implement step
+  cy.get('.seats-explained > :nth-child(3)').should('be.visible');
+});
---
+++ specs/step_definitions/US-4-detailed page.js
@@ -0,0 +1,44 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Given('The user is on the websites home page', () => {
+  // TODO: implement step
+  cy.visit('https://filmvisarna-team5.nodehill.se/');
+});
+
+Given('The user click on the movie titled {string}', (movie) => {
+  // TODO: implement step
+  cy.wait(4000);
+  cy.get(':nth-child(1) > .movieImages').first().click({force: true});
+});
+
+Then('The user should be directed to the movie details page', () => {
+  // TODO: implement step
+  cy.wait(2000);
+  cy.get('.movie-details-component').should('be.visible');
+});
+
+When('The user look at the page content', () => {
+  // TODO: implement step
+  cy.get('div#root div.col-lg-6.col-xl-7.col-xxl-8 > div').should('be.visible');
+});
+
+Then('The user should see the following {string}' ,  () => {
+  // TODO: implement step
+  cy.get('iframe').should('be.visible');
+  cy.get('.movie-details-director > p > .text-secondary-detail-color').should('be.visible');
+  cy.get('div#root div.movie-details-age-rest > p.text-primary-detail-color').should('be.visible');
+  cy.get('div#root div:nth-child(8) > p > span').should('be.visible')
+  cy.get('div#root div.movie-details-desc').should('be.visible');
+  cy.get('div#root div.movie-details-actors > p > span').should('be.visible');
+
+});
+
+Then('The user click on the {string} button or a smilar navigation element', (a) => {
+  // TODO: implement step
+  cy.get('.company-logo').click();
+});
+
+Then('The user should be redirected back to the home page', () => {
+  // TODO: implement step
+  cy.visit('https://filmvisarna-team5.nodehill.se/');
+});
