ba517eb306b4ad6dcf4c29f4616eefb2361a254e
gursell <unlgrsel@gmail.com>
2024-02-04 23:29:54 +0100

    gursell

--- specs/step_definitions/US_15.js
+++ specs/step_definitions/US_15.js
@@ -6,8 +6,9 @@ Given('The user is at the home page', () => {
 });
 
 Then('The user clicks to symbolized button at the right top side of website', () => {
-  cy.get('.header').click();
+  cy.get('#basic-nav-dropdown > .bi').click();
 });
+
 Then('The user clicks to the {string} button', (buttonText) => {
   cy.get('[href="/logga-in"]').click();
 });
@@ -18,26 +19,21 @@ Then('The user logs in with the {string} address and {string}.', (email, passwor
   cy.get('#login-btn').click();
 });
 
+When('The user can see the booking history under {string}', (sectionText) => {
+  cy.get(':nth-child(3) > h2').should('be.visible');
+});
+
 Given('The user clicks to {string} button', (buttonText) => {
   cy.get(':nth-child(4) > .justify-content-center > .cancel-booking-btn').click();
+  // cy.get('.row > :nth-child(5)').click();
 });
 
-Given('The user clicks to {string} link under upper right link/box', (linkText) => {
-  cy.get('.upper-right-box').contains(linkText).click();
+Given('The user can click to {string} button in the next process', (buttonText) => {
+  cy.get('.modal-cancel-btn').click();
 });
 
-When('The user can see the booking history under {string}', (sectionText) => {
-  cy.contains(sectionText).should('be.visible');
-});
-
-Given('The user can click to {string} button', (buttonText) => {
-  cy.contains(buttonText).click();
-});
-
-Then('the user click on the {string} button 2 times to redirect to home page.', (buttonText) => {
-  cy.contains(buttonText).click().click();
-});
 
 Then('the user should be redirected back to the home page.', () => {
-  cy.url().should('eq', 'https://example.com/home'); // Replace with the actual home page URL
+  cy.get('.company-logo'); // Replace with the actual home page URL
+  
 });
