a4213d233ab6146ea51d6b9fcdacc500c31dd761
Jensju <jens@modulmedia.se>
2024-02-05 14:31:47 +0100

    update the final version
    My three test for the final version

--- specs/step_definitions/reset-views-by-date.js
+++ specs/step_definitions/reset-views-by-date.js
@@ -3,15 +3,25 @@ import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 /* No duplicate steps, this one already in check-trailer-links.js
 Given('that I am on the start page', () => {});*/
 
+let toDaysDate = new Date().toLocaleDateString('sv-SV');
+
+Given('that the start date is todays date', () => {
+  cy.get('.filterScreenings').type(toDaysDate).should('exist');
+});
+
 Given('have choosen a new viewing date', () => {
   cy.get('.filterScreenings').type('2024-10-01').trigger('input');
 });
+
+Given('that I haver checked that the new date is accepted', () => {
+  cy.get('.filterScreenings').type('2024-10-01').should('exist');
+})
 
 When('I click on the reset button', () => {
   cy.get('.resetBtn').click();
 });
 
 Then('the calendar date will return to the origin state', () => {
-  let toDaysDate = new Date().toLocaleDateString('sv-SV');
+  // let toDaysDate = new Date().toLocaleDateString('sv-SV');
   cy.get('.filterScreenings').type(toDaysDate).should('exist');
 });
--- specs/views-by-date.feature
+++ specs/views-by-date.feature
@@ -1,16 +1,19 @@
 Feature: Filtering of views by date
   As a visitor on the webpage I expect to be able to select a specific start date for viewing upcoming movies to choose from and then make a choice from the film listed.
 
-  Scenario Outline: Choose a specific "<date>" from the date field
+  Background:
     Given that I am on the start page
     And choose a date in the calendar
+
+  Scenario Outline: Choose a specific "<date>" from the date field
     When I click the "<date>" in the calendar
-    Then I expect the "<date>" of the movie list to be updated
+    When the choice of movie list is updated and I choose the first element
+    Then I expect a button to apper to continue to the starting point for making a booking
 
     Examples:
       | date       |
-      | 2024-02-01 |
-      | 2024-02-12 |
-      | 2024-02-25 |
+      | 2024-02-10 |
+      | 2024-02-17 |
+      | 2024-02-24 |
       | 2024-03-01 |
-      | 2024-02-15 |
+      | 2024-02-08 |
